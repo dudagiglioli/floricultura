b5c5144c707a3cf06fcd409c175293b441aa0398
dudagiglioli <dudaguilice@gmail.com>
2023-10-06 11:53:36 -0300

    :confused:

---
+++ floricultura/App.tsx
@@ -0,0 +1,12 @@
+import {  SafeAreaView, StatusBar } from 'react-native';
+import Floricultura from './src/pages/Floricultura/components/index';
+
+export default function App() {
+
+
+  return (
+  <SafeAreaView>
+<StatusBar/>
+<Floricultura/>
+  </SafeAreaView>
+    );}
---
+++ floricultura/src/mocks/floricultura.tsx
@@ -0,0 +1,11 @@
+const floricultura = {
+    texto1: {
+        slogan: "A floricultura ideal para o melhor presente!"
+    },
+
+    texto2: {
+        descricao: "A floricultura ideal para encontrar o presente do seu amor, com diversas opções de cestas, flores e significados para presentear."
+    },
+}
+
+export default floricultura;
